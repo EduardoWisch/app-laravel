c809946d7d80afe1a4e2cb351ab24751436dcdc8
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-04-04 18:01:47 -0300

    Relacionamento One to Many - 12 minutos

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
 use App\Models\{
+    Course,
     User,
     Preference
 };
 use Illuminate\Support\Facades\Route;
 
+Route::get('/one-to-many', function() {
+    // $course = Course::create(['name' => 'Curso de Laravel']);
+    $course = Course::first();
+
+    $data = [
+        'name' => 'Modulo x2',
+    ];
+    $course->modules()->create($data);
+
+    $course->modules()->get();
+    $modules = $course->modules;
+
+    dd($modules);
+});
 
 Route::get('/one-to-one', function () {
     $user = User::with('preference')->find(2);
